e2e1a5cb1a2697b274c9ffa3d1bd4c64a9b7d9da
Sergey Petrachkov <petrachkovsergey@gmail.com>
2023-08-23 11:42:01 +0200

    Edit formula

--- Formula/xcresultscrapper.rb
+++ Formula/xcresultscrapper.rb
@@ -2,8 +2,8 @@ class Xcresultscrapper < Formula
     version "0.1b"
     desc "This is a tool that reads XCResult, parses it and produces different reports in different formats"
     homepage "https://github.com/SergeyPetrachkov/XCResultScrapper"
-    url "https://github.com/SergeyPetrachkov/XCResultScrapper/files/12417525/xcresultscrapper.tar.gz"
-    sha256 "62ffc3b23fd4e2e5ba4adbb008d96f1cd09a54855d6e297c642345a03ce91485"
+    url "https://github.com/SergeyPetrachkov/XCResultScrapper/files/12417742/xcresultscrapper.tar.gz"
+    sha256 "3830a44d798d0dc3dc9eea81588b50f304e8c37e3879796c67cb92b9bd3276c4"
   
     def install
       # Installation commands here
